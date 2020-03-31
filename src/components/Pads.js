54c4087ff7d47925e858ff20b4e252799a48149e
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-01 00:09:52 +0100

    Add Pads

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -1,11 +1,30 @@
 import './css/Pads.css';
 import React from 'react';
+import mapping from './mapping/mapping';
+
 
 const Pads = () => {
 
-  return (
-    <div className='pads__container'>
+  const samplesFolder = process.env.PUBLIC_URL + '/samples/'
 
+  const pads = mapping.map(([k, sample]) => {
+    return (
+      <div key={k} className='single-pad__container flex-center'>
+        <div className='drum-pad flex-center' id={sample}>
+          <p>{k}</p>
+          <audio src={samplesFolder + sample}
+            preload=''
+            className='clip'
+            id={k}>
+          </audio>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className='pads__container flex-center'>
+      {pads}
     </div>
   );
 }
---
+++ src/components/mapping/mapping.js
@@ -0,0 +1,13 @@
+const mapping = [
+  ['Q', 'tom-l'],
+  ['W', 'tom-h'],
+  ['E', 'crash'],
+  ['A', 'open-hat'],
+  ['S', 'snare'],
+  ['D', 'rim'],
+  ['Z', 'closed-hat'],
+  ['X', 'kick-h'],
+  ['C', 'kick-s']
+];
+
+export default mapping;
