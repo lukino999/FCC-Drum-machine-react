b68b868c2c08c1b69f9342754335a7fa7bb37f7c
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-04-06 11:15:10 +0100

    Refactor DrumMachine into a functional component

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Display from './Display';
 import Pads from './Pads';
 
-export default class DrumMachine extends React.Component {
-  render() {
-    return (
-      <div id='drum-machine' className='drum-machine__container'>
-        <Display />
-        <Pads keypress={this.props.keypress} />
-      </div>
-    )
-  }
+const DrumMachine = (props) => {
+  return (
+    <div id='drum-machine' className='drum-machine__container'>
+      <Display />
+      <Pads keypress={props.keypress} />
+    </div>
+  )
 }
+
+export default DrumMachine;
